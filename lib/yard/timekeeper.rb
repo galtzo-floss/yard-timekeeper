738e90729ba46905a330498626cbd1654145621d
Peter H. Boling <peter.boling@gmail.com>
2026-04-11 01:34:49 -0600

    🎨 Apply kettle-jem template v1.0.0

--- lib/yard/timekeeper.rb
+++ lib/yard/timekeeper.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
+require "version_gem"
 require_relative "timekeeper/version"
 
+Yard::Timekeeper::Version.class_eval do
+  extend VersionGem::Basic
+end
 module Yard
   module Timekeeper
     class Error < StandardError; end
