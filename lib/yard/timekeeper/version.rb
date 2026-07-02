7d5a0b4f2b5e41bec7fff66fde122f8939522e84
Peter H. Boling <peter.boling@gmail.com>
2026-07-02 00:46:40 -0600

    🔖 Bump gem version

--- lib/yard/timekeeper/version.rb
+++ lib/yard/timekeeper/version.rb
@@ -3,7 +3,7 @@
 module Yard
   module Timekeeper
     module Version
-      VERSION = "0.2.2"
+      VERSION = "0.2.3"
     end
     VERSION = Version::VERSION # Traditional Constant Location
   end
