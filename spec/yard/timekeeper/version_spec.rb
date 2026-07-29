c7c489051c3fa65bc728a53492f7a13a152849dc
Peter H. Boling <peter.boling@gmail.com>
2026-07-29 00:51:24 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

--- spec/yard/timekeeper/version_spec.rb
+++ spec/yard/timekeeper/version_spec.rb
@@ -5,8 +5,11 @@ RSpec.describe Yard::Timekeeper::Version do
   it_behaves_like "a Version module", described_class
 
   it "executes the version file for coverage without redefining constants" do
-    path = File.expand_path("../../../lib/yard/timekeeper/version.rb", __dir__)
-    anonymous_namespace = AnonymousLoader.load(files: path)
+    paths = [
+      File.expand_path("../../../lib/yard/timekeeper/version.rb", __dir__),
+      File.expand_path("../../../lib/yard/timekeeper/version_gem.rb", __dir__)
+    ].select { |path| File.file?(path) }
+    anonymous_namespace = AnonymousLoader.load(files: paths)
 
     expect(anonymous_namespace::Yard::Timekeeper::Version::VERSION).to eq(described_class::VERSION)
   end
